c496008cb7e5b670143d0f3104b3d440149efe21
Nir Tuttnauer <nir.tuttnauer@gmail.com>
2024-06-13 19:42:10 +0300

    Update page.tsx

--- podify/app/page.tsx
+++ podify/app/page.tsx
@@ -2,25 +2,40 @@
 import { useEffect, useState } from "react";
 import { PodList } from "./PodList";
 import { useBook } from "@/contexts/BookContext";
+import { z } from "zod";
+
+// Define the Zod schema
+const podcastEpisodeSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  uri: z.string(),
+  release_date: z.string(),
+});
+
+// Define the type based on the schema
+type Episode = z.infer<typeof podcastEpisodeSchema>;
 
 export default function Home() {
-  const [episodes, setEpisodes] = useState<any[]>([]);
-  const [filteredEpisodes, setFilteredEpisodes] = useState<any[]>([]);
-  const { selectedBook, setSelectedBook } = useBook();
+  const [episodes, setEpisodes] = useState<Episode[]>([]);
+  const [filteredEpisodes, setFilteredEpisodes] = useState<Episode[]>([]);
+  const { selectedBook } = useBook();
+
   useEffect(() => {
     fetch("/api/podcast-episodes")
       .then((response) => response.json())
       .then((data) => {
         console.log("Fetched data:", data); // Check what the data looks like
-        if (data) {
-          // Ensure that data.items is an array
-          setEpisodes(data.items); // Use the 'items' array for episodes
-          setFilteredEpisodes(data.items); // Initialize filtered episodes with fetched data
+        if (data && Array.isArray(data.items)) {
+          try {
+            // Validate each item in the data array
+            const validEpisodes = data.items.map((item: any) => podcastEpisodeSchema.parse(item));
+            setEpisodes(validEpisodes); // Use the 'items' array for episodes
+            setFilteredEpisodes(validEpisodes); // Initialize filtered episodes with fetched data
+          } catch (e) {
+            console.error("Validation error:", e);
+          }
         } else {
-          console.error(
-            "Fetched data does not contain an array of items:",
-            data
-          );
+          console.error("Fetched data does not contain an array of items:", data);
         }
       })
       .catch((error) => console.error("Error fetching episodes:", error));
@@ -34,8 +49,7 @@ export default function Home() {
     }
   }, [selectedBook, episodes]); // Depend on selectedBook and episodes
 
-
-  const filterEpisodes = (episodes: any[], book: string) => {
+  const filterEpisodes = (episodes: Episode[], book: string) => {
     if (book === "0" || !Array.isArray(episodes)) {
       return episodes;
     } else {
